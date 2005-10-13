f05583e3dacff831f24ceaa13dabd3ecec99d3a7
sjoyeux <tailor@localhost.localdomain>
2005-10-13 09:22:16 +0000

    No commit message

--- test/testsuite.hh
+++ test/testsuite.hh
@@ -6,10 +6,10 @@ using namespace boost::unit_test;
 
 #if BOOST_VERSION < 103300
 #define BOOST_REQUIRE_THROW(s,c) BOOST_CHECK_THROW(s,c)
-void set_log_threshold_level(log_level lev)
+inline void set_log_threshold_level(log_level lev)
 { unit_test_log::instance().set_log_threshold_level(lev); }
 #else
-void set_log_threshold_level(log_level lev)
+inline void set_log_threshold_level(log_level lev)
 { unit_test_log.set_threshold_level(lev); }
 #endif
 
